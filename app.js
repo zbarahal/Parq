0f7d3ec5d81909fee53f6f6107b1444234d7d81d
Zach Barahal <barahal@usc.com>
2014-03-13 00:18:27 -0700

    More mongo fun

--- app.js
+++ app.js
@@ -6,22 +6,19 @@ var request = require('request');
 var async = require('async');
 
 var routes = require("./routes");
-
-// mongo 
-var mongo   = require('mongodb'), MongoClient = mongodb.MongoClient
-var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
-var DB = 'localhost:27017/ddparqdb'
-var COLL = 'spaces'
-mongo.Db.connect(mongoUri, function (err, db) {
-  db.collection('mydocs', function(er, collection) {
-    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er, rs) {
-    });
-  });
-});
-// create our app using express
 var app = express();
-
-spacesColl = mongo.db(DB, { safe: true }).collection(COLL)
+// mongo 
+var mongoose = require('mongoose');
+var mongoUri = /*'mongodb://heroku_app22854962:4lklfpqm26kinofbfla6m7alpk@ds033459.mongolab.com:33459/heroku_app22854962';*/ process.env.MONGOLAB_URI;
+mongoose.connect(mongoUri, function (err, res) {
+  if (err) {
+    console.log ('Error connecting to: ' + mongoUri + ' ' + err);
+  }
+  else {
+    console.log ('Succeeded');
+  }
+});
+//spacesColl = mongo.db(DB, { safe: true }).collection(COLL)
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -46,145 +43,6 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 
-app.get('/spots', function(req, res) {
-  spacesColl.find({}).toArray(function(err, items) {
-    if(err) {
-      return res.send(err);
-    }
-    var spots = {
-      "spots": items
-    }
-    res.json(spots);
-  });
-});
-
-// add to the database
-app.post('/add', function(req, res) {
-
-  // get the body of the request
-	body = req.body;
-
-  // get all fields
-	var name = body.name,
-		  address = body.address,
-		  price = body.price,
-		  startTime = body.startTime,
-		  endTime = body.endTime;
-
-  if (!name) {
-    errorResponse("Invalid /add: No Name", res);
-  }
-  if (!address) {
-    errorResponse("Invalid /add: No Address", res);
-  }
-  if (!price) {
-    errorResponse("Invalid /add: No Price", res);
-  }
-  if (!startTime) {
-    errorResponse("Invalid /add: No Start Time", res);
-  }
-  if (!endTime) {
-    errorResponse("Invalid /add: No End Time", res);
-  }
-
-  // all good, geocode
-
-  // debug output
-  console.log(name + ", " + address + ". "); 
-  console.log(price + " from " + startTime + " to " + endTime);
-
-  // build fields
-
-  var nextId = spacesColl.find().count(function(err,count){
-      nextId = count + 1; 
-  });
-  // geocodable address
-  var geoAddress = address.split(' ').join('+');
-
-  var domain = 'http://maps.googleapis.com/maps/api/geocode/json?';
-  var options = 'address=' + geoAddress + '&sensor=false';
-
-  console.log("hitting " + domain + options);
-  console.log("Test");
-
-  request(domain + options, function (error, response, body) {
-    
-    if (!error && response.statusCode == 200) {
-      var json = JSON.parse(body);
-      if (json.status === "OK") {
-        location = json.results[0].geometry.location;
-        formattedAddress = json.results[0].formatted_address;
-
-        // got our location, now enter into DB
-        var newItem = {
-          "UUID" : nextId,
-          "name" : name,
-          "address" : formattedAddress,
-          "latlng" : [ location.lat, location.lng],
-          "price" : price,
-          "startTime" : startTime,
-          "endTime" : endTime,
-          "reserved" : false
-        };
-        // add to spaces collection
-        spacesColl.insert(newItem, {}, function() {
-          res.send("Inserted space!");
-		  console.log("Inserted"); 
-        });
-
-
-        // assuming always confirmed
-        var response = {
-          "status": "confirmed"
-        };
-        res.json(response);
-
-      } else {
-        errorResponse("Invalid /add: Address couldn't be geocoded", res);
-      }
-    } else {
-      errorResponse("Invalid /add: Address couldn't be geocoded", res);
-    }
-  })  
-}); // end app.post()
-
-app.post('/reserve', function(req, res) {
-
-  body = req.body;
-
-  //has to be casted into an int...damnit
-  var id = parseInt(body.id,10);
-
-  if (!id) {
-    errorResponse("Invalid /reserve: No id", res);
-  }
-
-  spacesColl.update({UUID : id }, {$set:{reserved: true }}, function(err) {
-    if (err){ 
-      console.log("error updating"); 
-      // if not good to go
-      var response = {
-        "status": "error"
-      };
-      res.json(response);
-    }
-    else { 
-      console.log("reserving " + id +"...");
-      // if good to go
-      var response = {
-        "status": "confirmed"
-      };
-      res.json(response);
-    }
-  });
-
-  
-
-  
-  
-  
-
-});
 
 function errorResponse(error, res) {
   console.log(error);
@@ -193,7 +51,7 @@ function errorResponse(error, res) {
   }
   res.json(response);
 }
-
-var server = http.createServer(app).listen(app.get('port'), function(){
+var http = require('http');
+http.createServer(app).listen(app.get('port'), function(){
   console.log('Parking app server listening on port ' + app.get('port'));
 });
